77673ddca4877d66dc565994e1d00d9f6ed2580c
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-03-07 22:29:11 +0200

    second homework done, repaired

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -1,4 +1,4 @@
-import React, {SetStateAction} from 'react'
+import React from 'react'
 import Affair from './affair/Affair'
 import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
