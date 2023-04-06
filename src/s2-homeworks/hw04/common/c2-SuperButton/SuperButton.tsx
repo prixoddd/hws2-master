322114612b6a759dd934ce71c53014c4bcb97f1a
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-04-07 01:37:02 +0300

    hw6 done

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -25,7 +25,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
                 : xType === 'secondary'
                 ? ` ${s.secondary}`
                 : ` ${s.default}`)
-        // + (className ? ' ' + className : '') // задачка на смешивание классов
+
 
     return (
         <button
