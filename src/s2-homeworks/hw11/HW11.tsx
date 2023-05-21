b42cd8350d005fdcea78585f56b55124fa2216fe
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-05-21 18:45:20 +0300

    trying to solve HW 11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,7 +15,17 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    console.log(`Первый ${value1}`)
+    console.log(`Второй ${value2}`)
+
+    const change = (event: Event, value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+            // Обработка, если значение не является массивом
+        }
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
 
@@ -29,6 +39,10 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            value={value1}
+                            onChange={change}
+                            min={0}
+                            max={100}
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,6 +51,10 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            value={[value1,value2]}
+                            onChange={change}
+                            min={0}
+                            max={100}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
